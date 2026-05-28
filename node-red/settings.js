3b3dddcc7e6a4a2b524404adb3ae452f060ed861
M3XH4 <mechasean23123@gmail.com>
2026-05-29 00:20:15 +0800

    refactor: updated node-red

--- node-red/settings.js
+++ node-red/settings.js
@@ -1,6 +1,5 @@
 module.exports = {
   uiPort: process.env.PORT || 1880,
   disableEditor: false,
-  flowFile: "flows.json",
-  credentialSecret: "sentry-secret-key"
+  credentialSecret: process.env.NODE_RED_CREDENTIAL_SECRET || "sentry-secret",
 };
